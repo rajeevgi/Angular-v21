2e8dec227928ca3dba0485616f0613f7d7de57e9
Rajeev Gupta <rajivg216@gmail.com>
2026-05-15 14:31:02 +0530

    Changes in datepicker.

--- AngHijriGregorianDatePickerDemo/src/app/datepicker/ng-datepicker/ng-datepicker.ts
+++ AngHijriGregorianDatePickerDemo/src/app/datepicker/ng-datepicker/ng-datepicker.ts
@@ -1,20 +1,45 @@
-import { Component } from '@angular/core';
-import { HijriGregorianDatepickerModule } from 'angular-hijri-gregorian-datepicker';
+import { Component, ViewChild } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+import {
+  HijriGregorianDatepickerComponent,
+  HijriGregorianDatepickerModule,
+} from 'angular-hijri-gregorian-datepicker';
 
 @Component({
   selector: 'app-ng-datepicker',
   standalone: true,
-  imports: [HijriGregorianDatepickerModule],
+  imports: [HijriGregorianDatepickerModule, FormsModule],
   templateUrl: './ng-datepicker.html',
   styleUrl: './ng-datepicker.css',
 })
 export class NgDatepicker {
+  selectedDate: any = '';
+  selectedCal: 'greg' | 'umAlQura' = 'greg';
 
-  selectedDate : any = '';
+  @ViewChild('pikcer')
+  picker!: HijriGregorianDatepickerComponent;
 
-  onDateSelect(event : any){
-    console.log("Selected Date =>", event);
-    this.selectedDate = event;
+  onDateSelect(event: any) {
+    console.log('EVENT => ', event);
+
+    // detect current mode from component
+    const currentMode = this.picker.mode;
+
+    console.log('CURRENT MODE => ', currentMode);
+
+    // Gregorian
+    if (currentMode === 'greg') {
+      this.selectedDate = event.gD;
+    }
+
+    // Hijri
+    else {
+      this.selectedDate = event.uD;
+    }
   }
 
+  // confirm button click
+  onSubmit(event: any) {
+    console.log('Confirmed =>', event);
+  }
 }
