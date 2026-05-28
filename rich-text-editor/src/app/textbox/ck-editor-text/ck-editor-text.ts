262a3524152144e3ea22d4d065bc81617bd923d6
Rajeev Gupta <rajivg216@gmail.com>
2026-05-28 12:56:36 +0530

    Ck-Editor More Pluggins Added.

--- rich-text-editor/src/app/textbox/ck-editor-text/ck-editor-text.ts
+++ rich-text-editor/src/app/textbox/ck-editor-text/ck-editor-text.ts
@@ -1,50 +1,281 @@
 import { Component } from '@angular/core';
 import { FormsModule } from '@angular/forms';
-import { CKEditorModule } from '@ckeditor/ckeditor5-angular';
-import { Bold, ClassicEditor, Essentials, Font, FontBackgroundColor, FontColor, FontFamily, FontSize, Heading, Italic, Paragraph } from 'ckeditor5';
 
+import { CKEditorModule } from '@ckeditor/ckeditor5-angular';
+
+import {
+
+  ClassicEditor,
+
+  Essentials,
+  Paragraph,
+  Heading,
+
+  Bold,
+  Italic,
+  Underline,
+  Strikethrough,
+  Subscript,
+  Superscript,
+  Code,
+  CodeBlock,
+
+  Font,
+  FontFamily,
+  FontSize,
+  FontColor,
+  FontBackgroundColor,
+
+  Alignment,
+
+  List,
+  TodoList,
+
+  Indent,
+  IndentBlock,
+
+  BlockQuote,
+
+  Link,
+
+  Table,
+  TableToolbar,
+  TableProperties,
+  TableCellProperties,
+
+  Image,
+  ImageToolbar,
+  ImageCaption,
+  ImageResize,
+  ImageStyle,
+  ImageUpload,
+  Base64UploadAdapter,
+
+  MediaEmbed,
+
+  HorizontalLine,
+
+  SpecialCharacters,
+
+  FindAndReplace,
+
+  Highlight,
+
+  RemoveFormat,
+
+  Undo
+
+} from 'ckeditor5';
 
 @Component({
   selector: 'app-ck-editor-text',
+  standalone: true,
   imports: [FormsModule, CKEditorModule],
   templateUrl: './ck-editor-text.html',
-  styleUrl: './ck-editor-text.css',
+  styleUrls: ['./ck-editor-text.css']
 })
 export class CkEditorText {
 
-  public Editor: any = ClassicEditor;
+  public Editor = ClassicEditor;
 
-  editorContent = '';
+  public editorContent = `
+    <h2>Advanced CKEditor 🚀</h2>
 
-  config = {
+    <p>This is a TinyMCE-like setup.</p>
+  `;
+
+  public config = {
+
+    licenseKey: 'GPL',
 
     plugins: [
+
       Essentials,
       Paragraph,
+      Heading,
+
       Bold,
       Italic,
-      Heading,
+      Underline,
+      Strikethrough,
+      Subscript,
+      Superscript,
+      Code,
+      CodeBlock,
+
       Font,
       FontFamily,
       FontSize,
       FontColor,
-      FontBackgroundColor
+      FontBackgroundColor,
+
+      Alignment,
+
+      List,
+      TodoList,
+
+      Indent,
+      IndentBlock,
+
+      BlockQuote,
+
+      Link,
+
+      Table,
+      TableToolbar,
+      TableProperties,
+      TableCellProperties,
+
+      Image,
+      ImageToolbar,
+      ImageCaption,
+      ImageResize,
+      ImageStyle,
+      ImageUpload,
+      Base64UploadAdapter,
+
+      MediaEmbed,
+
+      HorizontalLine,
+
+      SpecialCharacters,
+
+      FindAndReplace,
+
+      Highlight,
+
+      RemoveFormat,
+
+      Undo
     ],
 
     toolbar: [
+
       'undo',
       'redo',
+
       '|',
+
       'heading',
+
       '|',
+
       'fontFamily',
       'fontSize',
+
+      '|',
+
       'fontColor',
       'fontBackgroundColor',
-      '|',
-      'bold',
-      'italic'
-    ]
-  };
 
+      '|',
+
+      'bold',
+      'italic',
+      'underline',
+      'strikethrough',
+
+      '|',
+
+      'subscript',
+      'superscript',
+
+      '|',
+
+      'alignment',
+
+      '|',
+
+      'bulletedList',
+      'numberedList',
+      'todoList',
+
+      '|',
+
+      'outdent',
+      'indent',
+
+      '|',
+
+      'insertTable',
+
+      '|',
+
+      'link',
+
+      '|',
+
+      'uploadImage',
+
+      '|',
+
+      'mediaEmbed',
+
+      '|',
+
+      'blockQuote',
+      'codeBlock',
+
+      '|',
+
+      'horizontalLine',
+
+      '|',
+
+      'specialCharacters',
+
+      '|',
+
+      'highlight',
+
+      '|',
+
+      'removeFormat'
+    ],
+
+    table: {
+      contentToolbar: [
+        'tableColumn',
+        'tableRow',
+        'mergeTableCells',
+        'tableProperties',
+        'tableCellProperties'
+      ]
+    },
+
+    image: {
+      toolbar: [
+        'imageTextAlternative',
+        'toggleImageCaption',
+        'imageStyle:inline',
+        'imageStyle:block',
+        'imageStyle:side'
+      ]
+    },
+
+    fontFamily: {
+      options: [
+        'default',
+        'Arial',
+        'Courier New',
+        'Georgia',
+        'Tahoma',
+        'Times New Roman',
+        'Verdana'
+      ]
+    },
+
+    fontSize: {
+      options: [
+        10,
+        12,
+        14,
+        'default',
+        18,
+        20,
+        24,
+        30
+      ]
+    }
+  };
 }
